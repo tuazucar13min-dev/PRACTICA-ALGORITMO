e79da2cbfd63eb52e379816d8ec3e7c17940aa7a
tuazucar13min-dev <tuazucar13min@gmail.com>
2025-11-01 22:33:27 -0500

    SEMANA 13

---
+++ RecursividadYbacktracking/src/main/java/com/mycompany/recursividadybacktracking/RecursividadYbacktracking.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.recursividadybacktracking;
+
+/**
+ *
+ * @author CIELO
+ */
+public class RecursividadYbacktracking {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
