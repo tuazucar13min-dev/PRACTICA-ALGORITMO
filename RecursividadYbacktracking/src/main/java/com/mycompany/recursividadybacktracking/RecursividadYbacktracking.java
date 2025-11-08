423feea2a03bc7d4817591c10649d1fe62a21815
tuazucar13min-dev <tuazucar13min@gmail.com>
2025-11-08 00:54:46 -0500

    SEMANA 13

--- RecursividadYbacktracking/src/main/java/com/mycompany/recursividadybacktracking/RecursividadYbacktracking.java
+++ RecursividadYbacktracking/src/main/java/com/mycompany/recursividadybacktracking/RecursividadYbacktracking.java
@@ -9,8 +9,45 @@ package com.mycompany.recursividadybacktracking;
  * @author CIELO
  */
 public class RecursividadYbacktracking {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+// MANGAS_ANIME 
+   // Método recursivo para calcular precio total similar al factorial 
+    public double calcularTotalRecursivo(){
+    return calcularPrecioRecursivo(this.cantidad , this.precio);
     }
+    
+    public double calcularPrecioRecursivo(int cantidad, double precioUnitario){
+       if (cantidad == 0){
+           return 0;
+       }
+       if (cantidad == 1){
+           return precioUnitario;
+       }
+       else{
+       return precioUnitario + calcularPrecioRecursivo ( cantidad - 1, precioUnitario);
+   }
+   }
+   // Método Descuento por Cantidad 
+   public double calcularDescuentoRecursivo(){
+   return calcularDescuentoRecursivoHelper(this.cantidad, this.precio); 
+   }
+    private double calcularDescuentoRecursivoHelper (int cantidadRestante, double precioUnitario){
+    if (cantidadRestante <= 0){
+        return 0;
+    }
+    double precioMangaActual;
+    //Aplica el descuento segun la cantidad
+    if (cantidadRestante > 10 ){
+        precioMangaActual = precioUnitario * 0.8; // 20% D
+    }
+    else if (cantidadRestante > 5 ){
+        precioMangaActual = precioUnitario *0.9; // 10% D
+    }
+    else
+    {   precioMangaActual = precioUnitario ;
+    }
+    // ------------------------------------------
+    return precioMangaActual + calcularDescuentoRecursivoHelper ( cantidadRestante -1 , precioUnitario);
 }
+}
+   
+   
